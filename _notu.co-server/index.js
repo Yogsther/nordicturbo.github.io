b1eebb9001fb2257e0a5f46c9054149fd75fc8ee
Olle Kaiser <Olle@Livingfor.it>
2017-10-15 23:23:20 +0200

    Cached Chat on Notu.co , 10 latest messages show up on load.

--- _notu.co-server/index.js
+++ _notu.co-server/index.js
@@ -5,12 +5,14 @@ var socket = require("socket.io");
 var app = express();
 
 var server = app.listen(25565, function(){
+    
   console.log("Listening to requests on port 25565");
 });
 
 
 var admins = ["Agman", "🅱man", "Olle", "DigitalMole", "Pop", "Popkrull", "Hivod", "Olof"];
 var clients = [];
+var cache = [];
 
 // Static files
 
@@ -24,7 +26,47 @@ var io = socket(server);
 
 io.on("connection", function(socket){
   
-    var messages = "test"
+
+            
+       
+    // Get the 10 latest messages
+    // timeback is how many messages will get loaded for new users.
+    var timeBack = 10;
+    
+    var cacheMessage;
+    var lastMessagePos = cache.length;
+    var firstMessagePush = lastMessagePos - timeBack;
+
+    if(lastMessagePos > 0){
+    
+    var i = 0;
+    while(i < 10){
+        
+                
+        if(firstMessagePush < 0){
+            firstMessagePush++;
+        }
+        
+        if(firstMessagePush >= 0){
+            
+        cacheMessage = cache[firstMessagePush];
+        if(cacheMessage != null){
+            
+        io.sockets.connected[socket.id].emit("chat", cacheMessage);
+
+        console.log(cacheMessage.username);
+            
+        firstMessagePush++;
+            }
+            i++;
+        }
+    }
+    
+}
+    
+    console.log(lastMessagePos);
+    var messages = "boi";
+    
     console.log("User has connected: " + socket.id);
     io.sockets.connected[socket.id].emit("cache", messages);
 
@@ -32,7 +74,6 @@ io.on("connection", function(socket){
 socket.on('disconnect', function(){
     console.log('User has disconected');
   });
-
     
     
     
@@ -44,9 +85,20 @@ socket.on("chat", function(data){
         console.log("failed: too long username: " + data.username);
         return;
     }  
+    
+    // Save message to cache
+    cache.push(data);
     // Send out message to every client.
     io.sockets.emit("chat", data);
     // Log message in console.
     console.log("Message > " + data.username + ": " + data.message);
   });
 });
+
+
+
+
+
+
+
+
