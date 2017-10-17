4ffaba133e857c9391d5399798f802b773da1464
Olle Kaiser <Olle@Livingfor.it>
2017-10-17 20:48:54 +0200

    Added Fix

---
+++ _notu.co-server/index.js
@@ -0,0 +1,210 @@
+var express = require("express");
+
+var socket = require("socket.io");
+
+var app = express();
+
+var server = app.listen(25565, function(){
+    
+  console.log("Listening to requests on port 25565");
+});
+
+
+// var admins = ["Agman", "🅱man", "Olle", "DigitalMole", "Pop", "Popkrull", "Hivod", "Olof"];
+
+var onlineUsers = [];
+var cache = [];
+
+// Static files
+
+app.use(express.static("public"))
+
+// Socket setup
+
+var io = socket(server);
+
+
+
+
+
+io.on("connection", function(socket){
+    
+    
+    // Request user info on connection
+    io.sockets.connected[socket.id].emit("login", "loginInfo");
+       
+    // Get the 10 latest messages
+    // timeback is how many messages will get loaded for new users.
+    var timeBack = 10;
+    
+    var cacheMessage;
+    var lastMessagePos = cache.length;
+    var firstMessagePush = lastMessagePos - timeBack;
+
+    if(lastMessagePos > 0){
+    
+    var i = 0;
+    while(i < 10){
+        
+                
+        if(firstMessagePush < 0){
+            firstMessagePush++;
+        }
+        
+        if(firstMessagePush >= 0){
+            
+        cacheMessage = cache[firstMessagePush];
+        if(cacheMessage != null){
+            
+        io.sockets.connected[socket.id].emit("chat", cacheMessage);
+
+       
+            
+        firstMessagePush++;
+            }
+            i++;
+        }
+    }
+    
+}
+    
+
+
+    
+socket.on('disconnect', function(){
+    
+    
+    // Delete user from online list on disconnect
+    io.sockets.emit("listreset");
+    
+    var pos = onlineUsers.findIndex(i => i.id === socket.id);
+    onlineUsers.splice(pos,1);
+    
+    var userPos = 0;    
+    
+    
+    // Send all online users
+
+    while(userPos <= 50){
+
+        if(onlineUsers[userPos] == undefined){
+            userPos++;
+        }
+        if(onlineUsers[userPos] != undefined){
+        var newUser = onlineUsers[userPos];
+        io.sockets.emit("onlinepush", newUser);
+        userPos++;
+        }
+    }
+    
+    
+    
+  });
+    
+    
+    
+    
+socket.on("chat", function(data){
+    
+    if(data.username == null){
+        console.error("Error: Null username");
+        return;
+    }
+    // Check for too long usernames (hacked!)
+    if(data.username.length > 32){
+        console.error("failed: too long username: " + data.username);
+        return;
+    }  
+    
+    // Save message to cache
+    cache.push(data);
+    // Send out message to every client.
+    io.sockets.emit("chat", data);
+    // Log message in console.
+    console.log("Message > " + data.username + ": " + data.message);
+  });
+    
+    
+    
+    // Emit users
+    socket.on("sentover", function(userinfo){
+       
+    // NOTE! This system only supports 50 users online at a time. That can be changed, but will slow down performance.
+    // If desired, change here:
+    var pushPos = 10;
+    var namePushed = false;  
+    var supportedAmount = 50; 
+    io.sockets.emit("listreset"); 
+        
+    if(onlineUsers.findIndex(x => x.username == userinfo.username) == -1){
+
+    // Custom push feature for pushing client userstats - Prevents overwriting, happened when using .push    
+    while(namePushed == false){
+        
+        if(onlineUsers[pushPos] == undefined || onlineUsers[pushPos] == null){
+            console.log("Wrote in, on pos: " + pushPos);
+            onlineUsers[pushPos] = userinfo;
+            namePushed = true;
+        } 
+        if(onlineUsers[pushPos] != undefined && onlineUsers[pushPos] != null){
+            pushPos++; 
+            console.log("Skipped one");
+        }
+    }
+} 
+     /*
+        console.log("-------------------");
+    for(i = 0; i < 6; i++){
+        console.log("NUMBER: " + i + " >>> "+onlineUsers[i]);
+        if(onlineUsers[i] == undefined){
+            console.log(" == UNDEFINED");
+        } else {   
+        console.log("Defined.")
+        console.log("Username: " + onlineUsers[i].username);
+        console.log("ID: " + onlineUsers[i].id);
+        }
+        console.log("..............");
+    } */
+         
+        // onlineUsers.push(userinfo);
+        var userPos = 0; 
+    
+        // Send all online users
+
+        while(userPos <= supportedAmount){
+            
+        if(onlineUsers[userPos] == undefined){
+            userPos++;
+        }
+        if(onlineUsers[userPos] != undefined){
+            var newUser = onlineUsers[userPos];
+            io.sockets.emit("onlinepush", newUser);
+            userPos++;
+        }
+        /* Causes error!
+        if(onlineUsers[userPos] == null){
+            userPos = -1;
+            console.log("User pos: " + userPos);
+            console.log("Online users: " + onlineUsers.length);
+            return;
+        }
+        */
+    }
+        
+        
+});
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ chat.js
@@ -0,0 +1,286 @@
+
+// Socket.io & chat functions
+
+var socket = io.connect("http://213.66.254.63:25565");
+
+// On connection send over Username, ProfileLoc & Lvl
+
+socket.on("login", function(request){
+    
+    if(request == "loginInfo"){
+        
+        var messageUsername = readCookie("username");
+        var messageProfile = readCookie("profileLocation");
+        
+        if(messageUsername == null){
+        // Generate & save profile name, user has no saved Name.
+        var newRandom = Math.floor(Math.random()*8999)+1000;
+        var username = "New #" + newRandom;
+        createCookie("username", username, 10000);
+        createCookie("profileLocation", "http://livingforit.xyz/img/profiles/profile_none.png", 10000)
+        messageUsername = username;
+        messageProfile = "http://livingforit.xyz/img/profiles/profile_none.png";
+
+    }
+        
+        var xp = (readCookie("xp") / 1000) + 1;
+        xp = Math.floor(xp);
+        
+        
+        socket.emit("sentover", {
+            username: messageUsername,
+            profilepic: messageProfile,
+            xp: xp,
+            id: socket.id,
+            status: "Online"
+        });
+        
+        console.log("Sent over data");
+    }
+    
+   
+    
+}); 
+
+
+var onlineUsers = 0;
+
+socket.on("listreset", function(profile){
+   
+        onlineUsers = 0;
+        document.getElementById("online_list_inner").innerHTML = "";
+})
+
+socket.on("onlinepush", function(profile){
+    
+    onlineUsers++;
+    
+    // Get online users
+    
+    var data = profile;
+    
+    var xpColor = "white";
+    
+    if(data.xp >= 2){
+        xpColor = "#7c4fd1";
+    }
+    if(data.xp >= 3){
+        xpColor = "#ba1da7";
+    }
+    if(data.xp >= 4){
+        xpColor = "#a8174c";
+    }
+    if(data.xp >= 5){
+        xpColor = "#ce0c3c";
+    }
+    if(data.xp >= 6){
+        xpColor = "#ff4774";
+    }
+    if(data.xp >= 7){
+        xpColor = "#326617";
+    }
+    if(data.xp >= 8){
+        xpColor = "#5bc425";
+    }
+    if(data.xp >= 9){
+        xpColor = "#75f931";
+    }
+    if(data.xp >= 10){
+        xpColor = "#e0b731";
+    }
+    if(data.xp >= 15){
+        xpColor = "#f24e1d";
+    }
+    if(data.xp >= 20){
+        xpColor = "#f1bc1d";
+    }
+    if(data.xp >= 30){
+        xpColor = "#ff3030";
+    }
+    if(data.xp >= 40){
+        xpColor = "#e0b731";
+    }
+    if(data.xp >= 50){
+        xpColor = "#d7e1f2";
+    }
+    
+    console.log(xpColor);
+    document.getElementById("online_list_inner").innerHTML += '<div id="online_user_div"><img src="' + profile.profilepic + '" id="online_list_user_profile"><span id="online_list_name">' + profile.username + '</span><span id="online_list_status"><span style="color: ' + xpColor + ';">Lvl ' + profile.xp + ' </span>| <span style="color: #1ff226;">' + profile.status + '</span></span></div>';
+    
+    document.getElementById("online_users_number").innerHTML = onlineUsers;
+    
+    console.log("ONLINE USERS: " + onlineUsers);
+    console.log("USER ID: " + profile.id);
+    console.log("Online users: " + profile.username);
+    
+});
+
+
+// Listen for enter press to send message
+var sendBtn = document.getElementById("chat_message");
+sendBtn.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {  
+        sendMessage();
+    }
+});
+
+
+
+// Send message
+
+function sendMessage(){
+    
+    var currentdate = new Date(); 
+    
+    
+    
+    
+    
+    var messageContent = document.getElementById("chat_message").value;
+    var messageUsername = readCookie("username");
+    var messageProfile = readCookie("profileLocation");
+    
+    var hours = ("0" + currentdate.getHours()).slice(-2);
+    var minutes = ("0" + currentdate.getMinutes()).slice(-2);
+    
+    var messageTime = hours + ":" + minutes;
+    var xp = (readCookie("xp") / 1000) + 1;
+        xp = Math.floor(xp);
+    
+    document.getElementById("chat_message").value = ""; 
+    
+    // Dont send message if message is empty.
+    if(messageContent.length < 1){
+        return;
+    }
+    
+    if(chatEnabled == false){
+        // "Disable chat" message
+        document.getElementById("chat-inner").innerHTML += '<div id="spam_div"><span id="spam_warning"><i>Chat has been disabled for 5 seconds. <br>Reason: Spam.</i></span></div>';
+        
+        // Scroll down in chat on error message
+        var chatWindow = document.getElementById("chat-inner");
+        chatWindow.scrollTop = chatWindow.scrollHeight;
+        
+        return;
+    }
+    
+    
+    if(spamStatus >= 4){
+        // Disable chat
+        // "Disable chat" message
+        document.getElementById("chat-inner").innerHTML += '<div id="spam_div"><span id="spam_warning"><i>Chat has been disabled for 5 seconds. <br>Reason: Spam.</i></span></div>';
+        
+        // Scroll down in chat on error message
+        var chatWindow = document.getElementById("chat-inner");
+        chatWindow.scrollTop = chatWindow.scrollHeight;
+        
+        chatEnabled = false;
+        setTimeout(reviveChat, 5000);
+        return;
+        
+    }
+    
+    
+    // Message is clear to send
+    
+    spamStatus++;
+    
+    console.log(messageContent + messageProfile + messageUsername + messageTime + " ! " +  xp);
+    
+   
+    
+    socket.emit("chat", {
+        message: messageContent,
+        username: messageUsername,
+        profile: messageProfile,
+        time: messageTime,
+        xp: xp
+    });
+    
+}
+
+var chatEnabled = true;
+var clearToSend = true;
+var spamStatus = 0;
+
+setInterval(spamCheck, 2000);
+
+function spamCheck(){
+    
+    if(spamStatus >= 1){
+        spamStatus = spamStatus - 1;
+        console.log("Spam status*: " + spamStatus);
+        return;
+    }
+        console.log("Spam status: " + spamStatus);
+    
+    
+    
+}
+
+function reviveChat(){
+    chatEnabled = true;
+    spamStatus = 0;
+    console.log("Chat revived");
+}
+
+
+// Get incoming messages
+
+socket.on("chat", function(data){
+    
+    var xpColor = "white";
+    if(data.xp >= 2){
+        xpColor = "#7c4fd1";
+    }
+    if(data.xp >= 3){
+        xpColor = "#ba1da7";
+    }
+    if(data.xp >= 4){
+        xpColor = "#a8174c";
+    }
+    if(data.xp >= 5){
+        xpColor = "#ce0c3c";
+    }
+    if(data.xp >= 6){
+        xpColor = "#ff4774";
+    }
+    if(data.xp >= 7){
+        xpColor = "#326617";
+    }
+    if(data.xp >= 8){
+        xpColor = "#5bc425";
+    }
+    if(data.xp >= 9){
+        xpColor = "#75f931";
+    }
+    if(data.xp >= 10){
+        xpColor = "#e0b731";
+    }
+    if(data.xp >= 15){
+        xpColor = "#f24e1d";
+    }
+    if(data.xp >= 20){
+        xpColor = "#f1bc1d";
+    }
+    if(data.xp >= 30){
+        xpColor = "#ff3030";
+    }
+    if(data.xp >= 40){
+        xpColor = "#e0b731";
+    }
+    if(data.xp >= 50){
+        xpColor = "#d7e1f2";
+    }
+
+    document.getElementById("chat-inner").innerHTML += '<div id="message_blob"><img id="message_profile" src="' + data.profile + '"><span id="message_username">' + data.username + ' | <i><span style="color: ' + xpColor +';">' + data.xp + '</span></i></span><div id="message_content">' + data.message + '<div id="sidebar-colored" style="background-color: ' + xpColor + '; "></div><div id="timestamp">' + data.time + '</div></div></div>';
+    
+    
+    
+    // Scroll to bottom on new message
+    var chatWindow = document.getElementById("chat-inner");
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+
+});
+
