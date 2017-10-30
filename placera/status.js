124e073d25f8a9d2b978f2a4cd8210bdea3f3615
Olle Kaiser <Olle@Livingfor.it>
2017-10-30 14:09:00 +0100

    Placera status test

---
+++ placera/status.js
@@ -0,0 +1,41 @@
+
+// Socket.io & chat functions
+var socket = io.connect("http://213.66.254.63:25565");
+
+var playStatus = "Placera";
+
+// On connection send over Username, ProfileLoc & Lvl
+
+socket.on("login", function(request){
+
+    if(request == "loginInfo"){
+
+        var personalID = readCookie("persID");
+        // Generate new personalID for new users.
+        if(personalID == null){
+            var newID = Math.floor(Math.random() * 999999999) + 1;
+            createCookie("persID", newID, 10000);
+            personalID = readCookie("persID");
+        }
+
+        console.log("Personal ID: " + personalID);
+
+
+        var messageUsername = readCookie("username");
+        var messageProfile = readCookie("profileLocation");
+        var xp = (readCookie("xp") / 1000) + 1;
+        xp = Math.floor(xp);
+
+
+         socket.emit("sentover", {
+            username: messageUsername,
+            profilepic: messageProfile,
+            xp: xp,
+            id: socket.id,
+            status: playStatus,
+            persID: personalID
+        });
+
+        console.log("Sent over data");
+    }
+});
